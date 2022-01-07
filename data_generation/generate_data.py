7b466c5900bf83bc2e63588caab6241fdb8230cd
unknown <noamjanco@gmail.com>
2022-01-07 10:55:49 +0200

    generate data added for Z/2

---
+++ data_generation/generate_data.py
@@ -0,0 +1,17 @@
+import numpy as np
+import typing
+
+def generate_data_z_over_2(N: int,  Lambda: float) -> [np.array, np.array]:
+    """
+    Generate target vector x and measurements Y, in Z/2 scenario.
+
+    :param N: Number of observations, also the length of the vector x
+    :param Lambda: Signal-to-noise ratio (SNR) parameter
+    :return: x - vector we wish to estimate, x in {+-1}^N,
+             Y - N x N relative measurements matrix
+    """
+    x = np.expand_dims(2*(np.random.randint(0,2,N))-1,axis=-1)
+    a = np.random.randn(N, N)
+    W = np.tril(a) + np.tril(a, -1).T
+    Y = Lambda / N * x@x.T + 1/np.sqrt(N) * W
+    return x, Y
