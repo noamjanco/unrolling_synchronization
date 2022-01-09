930762340008adb7d641e534dac2492e42909c81
unknown <noamjanco@gmail.com>
2022-01-09 08:15:55 +0200

    added new experiments for z/2 bsc and antipodal signals

--- data_generation/generate_data.py
+++ data_generation/generate_data.py
@@ -41,6 +41,47 @@ def generate_training_data_z_over_2(N: int, Lambda: float, R: int) -> [np.array,
     return x_total, Y_total
 
 
+def generate_data_z_over_2_bsc(N: int, Lambda: float) -> [np.array, np.array]:
+    """
+    Generate target vector x and measurements Y, in Z/2 scenario.
+
+    :param N: Number of observations, also the length of the vector x
+    :param Lambda: Signal-to-noise ratio (SNR) parameter
+    :return: x - vector we wish to estimate, x in {+-1}^N,
+             Y - N x N relative measurements matrix
+    """
+    # noinspection PyTypeChecker
+    x = np.expand_dims(2*(np.random.randint(0,2,N))-1,axis=-1)
+    p_err = min(1/(2*Lambda),0.5)
+    a = np.random.choice([1, -1], (N, N), replace=True, p=[1 - p_err, p_err])
+    W = np.tril(a) + np.tril(a, -1).T
+    Y = Lambda / N * x @ x.T * W
+    return x, Y
+
+
+def generate_training_data_z_over_2_bsc(N: int, Lambda: float, R: int) -> [np.array, np.array]:
+    """
+    Generate training data for Z/2, the training data consists of R trials
+    :param N: Number of observations in each trial, also the length of the vector x
+    :param Lambda: Signal-to-noise ratio (SNR) parameter
+    :param R: Number of trials
+    :return: x_total - set of vector we wish to estimate, x in {+-1}^N,
+             Y_total - R x N x N relative measurements matrix
+    """
+    Y_total = []
+    x_total = []
+    for r in range(R):
+        x, Y = generate_data_z_over_2_bsc(N, Lambda)
+        x_total.append(x)
+        Y_total.append(Y)
+
+    x_total = np.asarray(x_total)
+    Y_total = np.asarray(Y_total)
+
+    print('Finished generating training data')
+    return x_total, Y_total
+
+
 def generate_data_gaussian(N: int, Lambda: float, L: int) -> [np.array, np.array, np.array]:
     """
     Generate target rotations vector x and relative rotations Y, and discrete matching shifts in U(1) gaussian scenario.
@@ -60,3 +101,55 @@ def generate_data_gaussian(N: int, Lambda: float, L: int) -> [np.array, np.array
     W = np.tril(a) + np.conj(np.tril(a, -1).T)
     Y = Lambda / N * x@np.conj(x.T) + 1/np.sqrt(N) * W
     return x, Y, s
+
+
+def generate_data_antipodal(N: int, Lambda: float, L: int) -> [np.array, np.array]:
+    """
+    Generate target flip vector s and relative measurements H in U(1) antipodal signal model.
+
+    :param N: Number of observations, also the length of the vector x
+    :param Lambda: Signal-to-noise ratio (SNR) parameter
+    :param L: Number of possible rotations - corresponds to the length of a vector in MRA experiments.
+    :return: s - vector we wish to estimate, x in {+-1}^N,
+             H - N x N relative measurements matrix
+    """
+    x = np.random.randn(L)
+    y = np.zeros((L, N))
+    n = np.zeros((L, N))
+    s = np.zeros((N,), dtype=int)
+    sigma = 1/Lambda
+
+    for i in range(N):
+        s[i] = 2*int(np.random.randint(0, 2)) - 1
+        n[:, i] = sigma * np.random.randn(L)
+        y[:, i] = x * s[i] + n[:, i]
+
+    H = np.zeros((N, N),dtype=np.float32)
+    for n in range(N):
+        for m in range(N):
+            H[n, m] = Lambda / N * y[:,n] @ y[:,m].T
+
+    return np.expand_dims(s,axis=-1).astype(np.float32), H
+
+
+def generate_training_data_antipodal(N: int, Lambda: float, R: int, L: int) -> [np.array, np.array]:
+    """
+    Generate training data for Z/2, the training data consists of R trials
+    :param N: Number of observations in each trial, also the length of the vector x
+    :param Lambda: Signal-to-noise ratio (SNR) parameter
+    :param R: Number of trials
+    :return: x_total - set of vector we wish to estimate, x in {+-1}^N,
+             Y_total - R x N x N relative measurements matrix
+    """
+    Y_total = []
+    x_total = []
+    for r in range(R):
+        x, Y = generate_data_antipodal(N, Lambda, L)
+        x_total.append(x)
+        Y_total.append(Y)
+
+    x_total = np.asarray(x_total)
+    Y_total = np.asarray(Y_total)
+
+    print('Finished generating training data')
+    return x_total, Y_total
---
+++ models/unrolling_synchronization_antipodal.py
@@ -0,0 +1,88 @@
+import keras
+import tensorflow as tf
+from keras.layers import Dense
+from keras import Model
+import datetime
+import numpy as np
+
+
+class NonLinearActivation(keras.layers.Layer):
+    def __init__(self):
+        super(NonLinearActivation, self).__init__()
+        self.dense_1 = Dense(1,kernel_initializer=tf.keras.initializers.ones())
+        self.dense_3 = Dense(1,kernel_initializer=tf.keras.initializers.zeros())
+        self.dense_5 = Dense(1,kernel_initializer=tf.keras.initializers.zeros())
+
+    def call(self, x):
+        y = self.dense_1(x) + self.dense_3(tf.math.pow(x,3)) + self.dense_5(tf.math.pow(x,5))
+        y = tf.math.minimum(y, tf.ones_like(y))
+        y = tf.math.maximum(y, -tf.ones_like(y))
+        return y
+
+
+class SynchronizationBlock(keras.layers.Layer):
+    def __init__(self, Lambda, N):
+        super(SynchronizationBlock, self).__init__()
+        self.dense_1 = Dense(1,kernel_initializer=tf.keras.initializers.ones())
+        self.Lambda = Lambda
+        self.N = N
+        self.nonlinear = NonLinearActivation()
+
+    def call(self, Y, x, x_prev, x_prev_prev):
+        x1 = self.Lambda * tf.matmul(Y, x)
+        # x1 = self.dense_1(x1)
+        # x2 = - self.Lambda ** 2 * tf.math.multiply(tf.expand_dims(1 - tf.reduce_mean(tf.pow(self.nonlinear2(x),2),axis=1),axis=-1), x_prev)
+        x2 = - self.Lambda ** 2 * tf.math.multiply(tf.expand_dims(1 - tf.reduce_mean(tf.pow(x, 2), axis=1), axis=-1),x_prev)
+        x_new = x1 + x2
+        x_new = self.nonlinear(x_new)
+
+        return x_new
+
+
+def loss_z_over_2(y_true,y_pred):
+    loss = 1 - tf.reduce_mean(tf.math.abs(tf.math.reduce_sum(tf.math.multiply(y_true,y_pred),axis=1)))/y_true.shape[1]
+    return loss
+
+
+def BuildModel(N,Lambda,DEPTH):
+    v_in = keras.layers.Input((N, 1))
+    v_in2 = keras.layers.Input((N, 1))
+    Y = keras.layers.Input((N, N))
+
+    v = v_in
+    v_prev = v_in2
+    v_prev_prev = v_in
+    v_new = []
+
+    for i in range(DEPTH):
+        v_new = SynchronizationBlock(Lambda,N)(Y, v, v_prev, v_prev_prev)
+        v_prev_prev = v_prev
+        v_prev = v
+        v = v_new
+
+
+    model = Model(inputs=[v_in,v_in2, Y], outputs=v_new)
+    opt = keras.optimizers.Adam(learning_rate=0.001) # working 18:07
+    model.compile(optimizer=opt, loss=loss_z_over_2)
+    model.summary()
+    return model
+
+
+def EvaluateModel(model, Y, x, x_init, x_init2):
+    x_est = model.predict([x_init, x_init2, Y])
+    x_est = tf.math.sign(x_est)
+    loss = loss_z_over_2(x, x_est)
+    print('[NN] loss = %lf' % loss)
+    return x_est, loss
+
+def TrainModel(model, Y, x, x_init, x_init2, Y_val,x_val,x_val_init,x_val_init2,epochs):
+    log_dir = "logs/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+    tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1, write_graph=True,
+                                                          write_images=True, profile_batch=0)
+    model.fit(x=[x_init,x_init2, Y],
+              y=x.astype(np.float32),
+              epochs=epochs,
+              validation_data=([x_val_init,x_val_init2, Y_val], x_val.astype(np.float32)),
+              validation_freq=20,
+              callbacks=[tensorboard_callback],
+              batch_size=500)
