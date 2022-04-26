8f41fd7d534005d74671a0afb4df26b45f3be5cb
unknown <noamjanco@gmail.com>
2022-04-26 21:46:10 +0300

    added ppm method for so(3)

--- data_generation/generate_data_so3.py
+++ data_generation/generate_data_so3.py
@@ -24,6 +24,14 @@ def project_to_orthogonal_matrix(M: np.array, flip=False):
     return m_proj
 
 
+def initialize_matrix(N: int) -> np.ndarray:
+    z_list = [project_to_orthogonal_matrix(np.random.randn(3,3), flip=True) for _ in range(N)]
+    z = np.zeros((3*N, 3))
+    for i in range(N):
+        z[3*i:3*i+3,:] = z_list[i]
+    return z
+
+
 def generate_data_so3(N: int, Lambda: float) -> [List[np.ndarray], np.ndarray]:
 
     H = np.zeros((3*N, 3*N))
@@ -62,6 +70,40 @@ def pim_so3(H: np.ndarray) -> List[np.ndarray]:
 
     return R_rec_proj
 
+def project(M: np.ndarray) -> np.ndarray:
+    """
+    This function projects each 3x3 block of M into the space of orthogonal matrices
+    :param M: 3N x 3 Matrix
+    :return: a projected matrix, 3N X 3
+    """
+    N = int(M.shape[0] / 3)
+    M_proj = np.zeros_like(M)
+    M_proj_list = [project_to_orthogonal_matrix(M[3*i:3*i+3,:]) for i in range(N)]
+    for i in range(N):
+        M_proj[3*i:3*i+3,:] = M_proj_list[i]
+    return M_proj
+
+
+def ppm_so3(H: np.ndarray, num_iterations: int = 200) -> List[np.ndarray]:
+    # Extract N from the size of H
+    N = int(H.shape[0] / 3)
+
+    # Initialize the matrix z
+    z = initialize_matrix(N)
+
+    for i in range(num_iterations):
+        z_tag = project(H @ z)
+        if np.linalg.norm(z_tag-z) < 1e-3:
+            break
+        z = z_tag
+
+
+    #todo: avoid this step by changing the return of PIM method
+    z_tag_list = []
+    for i in range(N):
+        z_tag_list.append(z_tag[3*i:3*i+3,:])
+    return z_tag_list
+
 def squared_correlation(X, X_hat) -> float:
 
     N = len(X)
@@ -78,17 +120,23 @@ if __name__ == '__main__':
     print('Main script')
     N = 100
     Lambda_range = np.arange(0.1,3,0.1)
-    correlations = []
+    correlations_pim = []
+    correlations_ppm = []
     for Lambda in Lambda_range:
         R,H = generate_data_so3(N,Lambda)
-        R_hat = pim_so3(H)
-        res = squared_correlation(R, R_hat)
-        correlations.append(res)
+        R_hat_pim = pim_so3(H)
+        R_hat_ppm = ppm_so3(H)
+        res_pim = squared_correlation(R, R_hat_pim)
+        res_ppm = squared_correlation(R, R_hat_ppm)
+        correlations_pim.append(res_pim)
+        correlations_ppm.append(res_ppm)
 
-    plt.plot(Lambda_range, correlations)
+    plt.plot(Lambda_range, correlations_pim, label='pim')
+    plt.plot(Lambda_range, correlations_ppm, label='ppm')
     plt.xlabel('SNR')
     plt.ylabel('Squared Correlation')
-    plt.title('Power Method')
+    plt.title('SO(3) Synchronization')
+    plt.legend()
     plt.show()
     print('finished')
 
